218ffd610e0f2657cc90ce0d0d8314b77fd35df7
aj1mm <avaijimmerson@gmail.com>
2018-07-13 16:21:23 +0000

    automatically backed up by learn

--- spec/least_coins_spec.rb
+++ spec/least_coins_spec.rb
@@ -1,14 +1,43 @@
-require_relative './spec_helper'
+def least_coins(cents)
+ coin_types = {
+   :quarters => 0,
+   :dimes    => 0,
+   :nickels  => 0,
+   :pennies  => 0
+ }
 
-describe 'LEAST COINS' do
-  
-  describe '#least_coins' do
-    it "returns the least amount of coins" do
-    
-    expect(least_coins(29)).to eq({:quarters => 1, :dimes => 0, :nickels => 0, :pennies => 4}) 
-
-    expect(least_coins(99)).to eq({:quarters => 3, :dimes => 2, :nickels => 0, :pennies => 4})
-    end
+ quarters = cents/25
+  if quarters == 0
+     coin_types[:quarters] = 0
+  else quarters != 0
+     coin_types[:quarters] = quarters
+     cents = cents - quarters*25
   end
+
+ dimes = cents/10
+  if dimes == 0
+     coin_types[:dimes] = 0
+  else dimes != 0
+     coin_types[:dimes] = dimes
+     cents = cents - dimes*10
+  end
+
+ nickels = cents/5
+  if nickels == 0
+     coin_types[:nickels] = 0
+  else nickels != 0
+     coin_types[:nickels] = nickels
+     cents = cents - nickels*5
+  end
+
+ pennies = cents
+  if pennies == 0
+     coin_types[:pennies] = 0
+  else pennies != 0
+     coin_types[:pennies] = pennies
+  end
+
+ puts coin_types
+coin_types
 
 end
